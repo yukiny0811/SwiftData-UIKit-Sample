85f21a6f71d4795dc9f1580148510ce33d8f70b6
yukiny0811 <yukiny0811@gmail.com>
2024-07-31 18:10:50 +0900

    Initial Commit

---
+++ SwiftDataSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftDataSample
+//
+//  Created by Yuki Kuwashima on 2024/07/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
